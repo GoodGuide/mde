4ca29d1f42ac9d6b5de4330e8e10d392454d816d
Ryan Taylor Long <ryan@rtlong.com>
2015-11-13 14:01:07 -0800

    Add remote_install.sh which does OS detection to grab the correct setup script

---
+++ remote_install.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# vim: set noexpandtab tabstop=4 shiftwidth=0:
+
+# This script is meant to be run on an unprovisioned environment via the familiar `curl | bash` (hack) style invocation. This is useful for systems before any of even the most basic tools are available, such as git.
+
+set -e -u
+
+red(){
+	echo -e "\n\x1b[31m$1\x1b[0m" >&2
+}
+
+onoes() {
+	red "$1"
+	exit 1
+}
+
+can_exec(){
+	silence command -v $1
+}
+
+silence() {
+	$@ >/dev/null 2>&1
+}
+
+download_from_URL() {
+	if can_exec curl; then
+		curl -fsSL "$1"
+	else
+		wget -q -O- "$1"
+	fi
+}
+
+exec_script_from_URL() {
+	exec bash <(download_from_URL "$1")
+}
+
+case "$(uname -s)" in
+	Darwin)
+		exec_script_from_URL 'https://raw.githubusercontent.com/GoodGuide/mde/master/setup_OSX.sh'
+	;;
+	Linux)
+		case "$(lsb_release -is)" in
+			Ubuntu)
+				exec_script_from_URL 'https://raw.githubusercontent.com/GoodGuide/mde/master/setup_Ubuntu.sh'
+			;;
+			*)
+				onoes "There is no automated setup script for your Linux distribution!"
+			;;
+		esac
+	;;
+	*)
+		onoes "There is no automated setup script for your OS!"
+	;;
+esac
---
+++ setup_Ubuntu.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# vim: set noexpandtab tabstop=4 shiftwidth=0:
+
+set -e -u
